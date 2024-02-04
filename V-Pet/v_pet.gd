cdf386d3efdbdbec377244cf029a2d1e76765ba2
Francisco Stanfield <158773050+stanly-freeman@users.noreply.github.com>
2024-02-03 21:29:12 -0500

    set up a basic happiness mechanic

--- V-Pet/v_pet.gd
+++ V-Pet/v_pet.gd
@@ -12,13 +12,11 @@ var is_alive = true
 
 
 func _on_timer_timeout():
-	needs.happiness -= 1
-	if needs.happiness < 1:
-		is_alive = false
+	if is_alive:
+		needs.happiness -= 1
+		if needs.happiness == 0:
+			is_alive = false
+		
+		
 	
-	#update_ui()
-	#update_behavoir()
-	print("timeout" + str(needs.happiness) + str(is_alive))
-
-
 
