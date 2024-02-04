179fbf6386163fda8c56c2ccfafd712a102aab33
Francisco Stanfield <158773050+stanly-freeman@users.noreply.github.com>
2024-02-03 23:45:39 -0500

    add ui functionality to show pet current status

--- V-Pet/v_pet.gd
+++ V-Pet/v_pet.gd
@@ -3,7 +3,7 @@ extends Node
 class_name v_pet
 
 @export var needs :  Dictionary = {
-	"happiness": 5,
+	"happiness": 10,
 	
 }
 var is_alive = true
@@ -14,9 +14,26 @@ var is_alive = true
 func _on_timer_timeout():
 	if is_alive:
 		needs.happiness -= 1
-		if needs.happiness == 0:
+		if needs.happiness > 5:
+			$Label.text = "Happy"
+		#elif needs.happiness < 3:
+			#$Label.text = "Sad"
+			#$AudioStreamPlayer.play()
+		elif needs.happiness < 1:
 			is_alive = false
+			$AudioStreamPlayer.stream = load("res://V-Pet/sound_effects/laser3.ogg")
+			$AudioStreamPlayer.play()
+			$Label.text = "Dead"
+			$Button.disabled = true
+		
 		
 		
 	
 
+
+
+func _on_button_pressed():
+	if is_alive:
+		needs.happiness += 5
+		if needs.happiness > 5:
+			$Label.text = "Happy"
